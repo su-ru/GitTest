ba006fa7d434c0f226ba747189fd30bc75e4ec45
Su Ru <su.ru@hotmail.com>
2020-05-31 00:08:38 -0400

    test

--- sample.py
+++ sample.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import numpy as np
 
+class Particle(IsDescription):
+    name      = StringCol(16)   # 16-character String
+    idnumber  = Int64Col()      # signed 64-bit integer
+    ADCcount  = UInt16Col()     # unsigned short integer
+    TDCcount  = UInt8Col()      # unsigned byte
+    grid_i    = Int32Col()      # integer
+    grid_j    = Int32Col()      # integer
+
+    # A sub-structure (nested data-type)
+    class Properties(IsDescription):
+        pressure = Float32Col(shape=(2,3)) # 2-D float array (single-precision)
+        energy   = Float64Col(shape=(2,3,4)) # 3-D float array (double-precision)
+
 if __name__ = "__main__":
 """ This is the main routine """
 # Hello World !
